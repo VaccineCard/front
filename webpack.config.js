e34032ab3761555be34c821e9ecfed132aa969a3
Acidiney Dias <acidineydias@gmail.com>
2019-04-23 13:19:49 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,11 @@
+var webpack = require('webpack')
+
 module.exports = {
-  mode: 'production'
+  // ...
+  plugins: [
+    // ...
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
+  ]
 }
